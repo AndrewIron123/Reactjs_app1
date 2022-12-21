a9b14aa03d09d176af4076bdab57ccd102407f82
AndrewIron123 <andrzejstal1991@gmail.com>
2022-12-21 01:11:18 +0100

    Add article_footer, comment main_footer

--- reactjs_app1/src/article/Article.jsx
+++ reactjs_app1/src/article/Article.jsx
@@ -13,7 +13,18 @@ function Article(props) {
 
     return (
       <>
-        <div className={"page" + (props.myClass ? (" "+ props.myClass) : "")} dangerouslySetInnerHTML={{__html: props.myHtml}} />
+        <div className={"article" + (props.myClass ? (" "+ props.myClass) : "")} dangerouslySetInnerHTML={{__html: props.myHtml}} />
+        <div className="article__footer">
+          <div className="article__footer_title">
+            Title: {props.myTitle}
+          </div>
+          <div className="article__footer_author">
+            Author: {props.myAuthor}
+          </div>
+          <div className="article__footer_date">
+            Date: {props.myDate}
+          </div>
+        </div>
       </>
     );
   }
--- reactjs_app1/src/layout/Layout.jsx
+++ reactjs_app1/src/layout/Layout.jsx
@@ -13,7 +13,7 @@ const Layout = ({ children }) => {
         <Content>
             {children}
         </Content>
-        <Footer />
+        {/* <Footer /> */}
     </>
     );
 };
