382fef915244bae281ae4e3dc34f2798ee8297e4
AndrewIron123 <andrzejstal1991@gmail.com>
2023-01-13 00:11:57 +0100

    Add fetch API in HomePage

--- reactjs_app1/src/HomePage/HomePage.jsx
+++ reactjs_app1/src/HomePage/HomePage.jsx
@@ -2,25 +2,41 @@ import Article from '../article/Article';
 import React, { useState, useEffect } from 'react';
 
 function HomePage() {
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [error, setError] = useState(null);
   const [data, setData] = useState({
     title: "",
     html: "",
     author: "",
     date: ""
   });
- 
+
+
   useEffect(() => {
-    console.log(process.env.REACT_APP_API_URL_ARTICLES);
-    const JSONFromAPI = '{"title":"simple title","html":"<div>simple text</div>", "author":"simple author", "date":"2022-12-20"}';
-    const JSONAsJsObject = JSON.parse(JSONFromAPI);
-    setData(JSONAsJsObject);
+    fetch(`${process.env.REACT_APP_API_URL_ARTICLES}/1`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsDataLoaded(true);
+          setData(result);
+        },
+        (error) => {
+          setIsDataLoaded(true);
+          setError(error);
+        }
+      )
   }, []);
 
 
-  
-  return (
-    <Article myHtml={data.html} myTitle={data.title} myAuthor={data.author} myDate={data.date} />
-  );
+  if (error) {
+    return <Article myHtml={`<div>${error.message}</div>`} myTitle={data.title} myAuthor={data.author} myDate={data.date} />;
+  } else if (!isDataLoaded) {
+    return <Article myHtml={"<div>Loading...</div>"} myTitle={data.title} myAuthor={data.author} myDate={data.date} />;
+  } else {
+    return (
+      <Article myHtml={data.html} myTitle={data.title} myAuthor={data.author} myDate={data.date} />
+    );
+  }
 }
 
-  export default HomePage;
+export default HomePage;
