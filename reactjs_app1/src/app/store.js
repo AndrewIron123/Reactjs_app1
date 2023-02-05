7d8cc8469ab3f70477403065b9811c9b9fffb4e1
AndrewIron123 <andrzejstal1991@gmail.com>
2023-02-06 00:15:16 +0100

    add handle localstorage with redux in change light/dark mode

--- reactjs_app1/src/app/store.js
+++ reactjs_app1/src/app/store.js
@@ -1,8 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit'
 import modeSwitchReducer from '../features/modeSwitch/modeSwitchSlice';
 
-export default configureStore({
+
+
+function loadDataFromStorage() {
+  try {
+    const data = localStorage.getItem("modeSwitch");
+    if (data){
+     return JSON.parse(data);
+    }
+  } catch (e) {
+    console.log(`$Caught error when trying to load data from storage: ${e}`);
+  }
+}
+
+
+function saveDataToStorage(data) {
+  try {
+    const dataAsString = JSON.stringify(data);
+    localStorage.setItem("modeSwitch", dataAsString);
+  } catch (e) {
+    console.log(`$Caught error when trying to add data to storage: ${e}`);
+  }
+}
+
+
+const store = configureStore({
   reducer: {
     modeSwitch: modeSwitchReducer
   }
-})
+}, loadDataFromStorage());
+
+store.subscribe(()=>saveDataToStorage(store.getState()));
+export default store;
--- reactjs_app1/src/features/modeSwitch/modeSwitchSlice.js
+++ reactjs_app1/src/features/modeSwitch/modeSwitchSlice.js
@@ -1,9 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+function loadDataFromStorage() {
+    try {
+        const data = localStorage.getItem("modeSwitch");
+        const dataAsObj = JSON.parse(data);
+        if (dataAsObj && dataAsObj.modeSwitch) {
+            const dataModeSw = dataAsObj.modeSwitch;
+            const initialMode = dataModeSw.mode;
+            return initialMode;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        console.log(`$Caught error when trying to load data from storage: ${e}`);
+        return false;
+    }
+}
+
 export const modeSwitchSlice = createSlice({
     name: 'modeSwitch',
     initialState: {
-        mode: false
+        mode: loadDataFromStorage()
     },
     reducers: {
         changeMode: state => {
